144da7c7246b738be6338fdbaa7e4d3f0242f431
Asher Nieffenegger <NieffAsh000@students.wvsd.org>
2022-01-14 15:15:22 -0800

    Added Drifferentail Drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Controller.Hand;
+import frc.robot.commands.ArcadeDriveCommand;
+import frc.robot.commands.TankDriveCommand;
 import frc.robot.commands.Autonomous.DriveForwardCommand;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -24,16 +26,17 @@ import frc.robot.subsystems.ShooterSubsystem;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
-  private final ClimberSubsystem m_climberSubsystem = new ClimberSubsystem();
-  private final IndexerSubsystem m_indexerSubsystem = new IndexerSubsystem();
-  private final IntakeSubsystem m_intakeSubsystem = new IntakeSubsystem();
-  private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
+  private final DrivetrainSubsystem Drivetrain = new DrivetrainSubsystem();
+  private final ClimberSubsystem climber = new ClimberSubsystem();
+  private final IndexerSubsystem indexer = new IndexerSubsystem();
+  private final IntakeSubsystem intake = new IntakeSubsystem();
+  private final ShooterSubsystem shooter = new ShooterSubsystem();
 
   private final Controller driverController = new Controller(0, Hand.Left, 0.05);
   private final Controller operatorController = new Controller(1, Hand.Left, 0.05);
 
   private final JoystickButton climberUpButton = driverController.getButton(Controller.Button.A);
+  private final JoystickButton shootCloseButton = operatorController.getButton(Controller.Button.A);
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
@@ -46,7 +49,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+
+    Drivetrain.setDefaultCommand(getArcadeDrive());
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -57,4 +63,13 @@ public class RobotContainer {
     // An ExampleCommand will run in autonomous
     return new DriveForwardCommand();
   }
+
+  private Command getTankDrive() {
+    return new TankDriveCommand(Drivetrain, () -> driverController.getRawAxis(5), () -> driverController.getRawAxis(0));
+ }
+
+ private Command getArcadeDrive() {
+    return new ArcadeDriveCommand(Drivetrain, () -> -driverController.getRawAxis(1), () -> driverController.getRawAxis(4));
+ }
+
 }
--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -1,9 +1,31 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
 
 public class DrivetrainSubsystem extends SubsystemBase {
-    public DrivetrainSubsystem(){
-        
+    MotorController leftMotor = new WPI_VictorSPX(DrivetrainConstants.DRIVETRAIN_LEFT_MOTOR_ID);
+    MotorController rightMotor = new WPI_VictorSPX(DrivetrainConstants.DRIVETRAIN_RIGHT_MOTOR_ID);
+
+    private final DifferentialDrive diffDrive = new DifferentialDrive(leftMotor, rightMotor);
+
+
+    public DrivetrainSubsystem () {
+
     }
+
+    public void tankDrive(double rightSpeed, double leftSpeed) {
+        diffDrive.tankDrive(leftSpeed, rightSpeed);
+    }
+
+    public void arcadeDrive(double xSpeed, double zRotation) {
+        diffDrive.arcadeDrive(xSpeed*DrivetrainConstants.DRIVE_SPEED_SCALE, zRotation*DrivetrainConstants.DRIVE_SPEED_SCALE);
+    }
+
 }
