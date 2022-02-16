dcdaf9315b1f9964ee90f8016b02de72890fa090
Brayden <chasebra001@students.wvsd.org>
2022-02-15 16:09:37 -0800

    Added encoder value stopper for climber

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -30,7 +30,10 @@ public class ClimberSubsystem extends SubsystemBase {
       climbPiston.set(Value.kForward);
     }
   }
-
+  public double getClimberEncoderValue(){
+    return climberMotor.getSelectedSensorPosition();
+  }
+  
   @Override
   public void periodic() { 
     // double climbSpeed = climbSpeedSupplier.getAsDouble();
