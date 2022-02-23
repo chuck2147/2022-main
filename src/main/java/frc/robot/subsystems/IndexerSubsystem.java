465388f2b16ca4891882aa8e2be2b376e596fd85
Asher Nieffenegger <NieffAsh000@students.wvsd.org>
2022-02-23 15:27:28 -0800

    Updated Indexer Subsystem

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -44,11 +44,51 @@ public class IndexerSubsystem extends SubsystemBase {
     return irUpper.getVoltage() > IndexerConstants.HOPPER_IR_VOLTAGE;
   }
 
+  /*
+When shooter is up to speed, both indexer motors feeds balls forward into the shooter.
+  else if lower sensor detects ball & upper sensor doesn't detect a ball, run both indexer motors upto upper sensor
+  else if intake is running & upper sensor detects a ball and the lower doesn't detect a ball, run lower index motor forwards only
+  else if intake is not running & upper sensor detects & ball and the lower doesn't detect a ball, don't run indexer
+  else if intake is running & neither sensor detects a ball, run both index motor forwards
+  else if intake is not running & neither sensor detects a ball, don't run indexer
+  else if intake is running reversed, run both index motors backwards
+  else if both sensors detect a ball, stop index motors
+
+
+
+
+  
+*/
   @Override
   public void periodic() {
     lowerMotor.set(ControlMode.PercentOutput, lowerSpeed);
     upperMotor.set(ControlMode.PercentOutput, upperSpeed);
     lowerSpeed = 0;
     upperSpeed = 0;
-  }
-}
+
+//     if "shooter is up to speed" {
+//       "run both indexer motors forward"
+//     }
+//     if else isLowerTriggered() "but upper sensor isn't triggered"{
+//       "run both indexer motors until upper sensor is triggered"
+//     }
+//     if else "intake is running and" isUpperTriggered() "but not lower sensor" {
+//       "run lower indexer motors only"
+//     }
+//     if else "intake is not running and" isUpperTriggered() "but not lower sensor" {
+//       "stop both indexer motors"
+//     }
+//     if else "intake is running and neither sensor detects a ball" {
+//       "run both indexer motors forward"
+//     }
+//     if else "intake is not running and neither sensor detects a ball" {
+//       "stop both indexer motors"
+//     }
+//     if else "intake is running backwards"{
+//       "run both indexer motors backwards"
+//     }
+//     else {
+//       "stop both indexer motors"
+//     }
+   }
+ }
